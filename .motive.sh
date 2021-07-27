150e93d2da736b0a2ed459ed605e037cf53ad457
__mr__ <69723444+1MahdiR@users.noreply.github.com>
2021-07-27 23:45:27 +0430

    Added 5 quotes

--- .motive.sh
+++ .motive.sh
@@ -16,6 +16,11 @@ QUOTES=( \
 "Keep calm and carry on" \
 "When you cease to dream you cease to live" \
 "There are no mistakes, only opportunities" \
+"It's going to be hard, but hard does not mean impossible" \
+"Success is the sum of small efforts, repeated day-in and day-out"
+"Your success and happiness lie in you"
+"Focus on your goal.\nDon't look in any direction but ahead." \
+"If you can dream it, you can do it" \
 )
 
 echo
